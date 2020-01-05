fbf110a096245c32a92d6350bca425bc214e4958
Hai Nguyen <hainv201gmail.com>
2020-01-05 23:15:48 +0100

    Update 4 rules and test file

--- ALE2/Word.cs
+++ ALE2/Word.cs
@@ -18,19 +18,24 @@ namespace ALE2
 
         public void IsWordAccepted(List<State> states, List<Transition> transitions)
         {
-            State Initial = states.Find(x => x.IsInitial);
-            int i = 0;
-            List<Transition> processedEpsilonTransitions = new List<Transition>();
-            if (transitions.Exists(x => x.GetPopStack() != null))
+            List<State> Initials = states.FindAll(x => x.IsInitial);
+            foreach (var Initial in Initials)
             {
-
-            }
-            else
-            {
-                IsAccepted = IsFinal(i, processedEpsilonTransitions, transitions,false, Initial);
+                int i = 0;
+                List<Transition> processedEpsilonTransitions = new List<Transition>();
+                if (transitions.Exists(x => x.GetPopStack() != null))
+                {
+                    List<Stack> stacks = new List<Stack>();
+                    IsAccepted = CheckWordIsAcceptedWhenTransitionHasStack(i, processedEpsilonTransitions, transitions, false, Initial, stacks);
+                }
+                else
+                {
+                    IsAccepted = IsFinal(i, processedEpsilonTransitions, transitions, false, Initial);
+                }
             }
         }
 
+        // assignment 2
         private List<Transition> PossibleMove(List<Transition> transitions, string alp, State current_State)
         {
             if (alp != "_")
@@ -45,7 +50,7 @@ namespace ALE2
             }
         }
 
-        private bool IsFinal(int i,List<Transition> processedEpsilonTransitions, List<Transition> transitions, bool UseEpsilonMove, State current_state)
+        private bool IsFinal(int i, List<Transition> processedEpsilonTransitions, List<Transition> transitions, bool UseEpsilonMove, State current_state)
         {
             string alp = "?";
             if (ContainOnlyEpsilon())
@@ -110,6 +115,80 @@ namespace ALE2
             {
                 return false;
             }
+        }
+
+        // assignment 6
+        private bool CheckWordIsAcceptedWhenTransitionHasStack(int i, List<Transition> processedEpsilonTransitions, List<Transition> transitions, bool UseEpsilonMove, State current_state, List<Stack> currentStacks)
+        {
+            List<Stack> child_stacks = currentStacks.ToList();
+            List<Transition> next_possible_transitions;
+            if (ContainOnlyEpsilon())
+            {
+                next_possible_transitions = ThirdRule(tran)
+            }
+            else
+            {
+                if (UseEpsilonMove)
+                {
+                    i -= 1;
+                }
+                if (i < Words.Length)
+                {
+                    alp = Words[i].ToString();
+                }
+            }
+            next_possible_transitions = PossibleMove(transitions, alp, current_state);
+            if (i == Words.Length && !ContainOnlyEpsilon() && current_state.IsFinal && child_stacks.Count == 0)
+            {
+                return true;
+            }
+            if (ContainOnlyEpsilon() && current_state.IsFinal && child_stacks.Count == 0)
+            {
+                return true;
+            }
+            next_possible_transitions = next_possible_transitions.Except(processedEpsilonTransitions).ToList();
+            if (next_possible_transitions.Count == 0)
+            {
+                return false;
+            }
+            foreach (Transition transition in next_possible_transitions)
+            {
+                if (transition.GetSymbol().Label == "_")
+                {
+                    UseEpsilonMove = true;
+                    processedEpsilonTransitions.Add(transition);
+                }
+                else
+                {
+                    UseEpsilonMove = false;
+                }
+                current_state = transition.GetRightState();
+                if (CheckWordIsAcceptedWhenTransitionHasStack(i + 1, processedEpsilonTransitions, transitions, UseEpsilonMove, current_state,child_stacks))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private List<Transition> FirstRule(List<Transition> transitions, string alp, State current_State, List<Stack> stacks)
+        {
+            return transitions.FindAll(x => x.GetLeftState() == current_State && x.GetSymbol().Label == alp && stacks.Contains(x.GetPopStack()));
+        }
+
+        private List<Transition> SecondRule(List<Transition> transitions, string alp, State current_State, List<Stack> stacks)
+        {
+            return transitions.FindAll(x => x.GetLeftState() == current_State && x.GetSymbol().Label == alp && (x.GetPopStack().Character == "_" || x.GetPopStack() == null));
+        }
+
+        private List<Transition> ThirdRule(List<Transition> transitions, State current_State, List<Stack> stacks)
+        {
+            return transitions.FindAll(x => x.GetLeftState() == current_State && x.GetSymbol().Label == "_" && stacks.Contains(x.GetPopStack()));
+        }
+
+        private List<Transition> LastRule(List<Transition> transitions, State current_State, List<Stack> stacks)
+        {
+            return transitions.FindAll(x => x.GetLeftState() == current_State && x.GetSymbol().Label == "_" && (x.GetPopStack().Character == "_" || x.GetPopStack() == null));
         }
     }
 }
